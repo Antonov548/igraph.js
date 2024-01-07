010692a3d58dc078af2766b69e001c2bae66659f
Antonov548 <antonov5551998@gmail.com>
2024-01-07 21:15:20 +0100

    fix test

--- tests/unit_tests/shortest.path.test.js
+++ tests/unit_tests/shortest.path.test.js
@@ -7,10 +7,22 @@ describe('igraph.js', () => {
     const g = igraph.create(v, 5, true);
     v.delete();
 
-    igraph.get_shortest_path(g, 0, 4, igraph.neimode.OUT);
+    const r = igraph.get_shortest_path(g, 0, 4, igraph.neimode.OUT);
 
-    // expect(vpath.data).toEqual(new Int32Array([0, 3, 4]));
-    // expect(epath.data).toEqual(new Int32Array([4, 3]));
+    expect(r.vertices.data).toEqual(new Int32Array([0, 3, 4]));
+    expect(r.edges.data).toEqual(new Int32Array([4, 3]));
+
+    g.delete();
+  });
+
+  it('shortest paths', async () => {
+    const igraph = global.igraph;
+
+    const v = new igraph.vector_int([0, 1, 0, 2, 1, 6, 2, 6, 1, 3, 1, 4, 1, 5,
+      3, 2, 4, 2, 5, 2]);
+
+    const g = igraph.create(v, 21, true);
+    v.delete();
 
     g.delete();
   });
